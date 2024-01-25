779c557e8e7a5e32d563654972d3418fdd3be09c
Qamar Ali <qamarali205@gmail.com>
2024-01-25 17:12:01 +0530

    ci/cd testing new

--- src/Constants/constant.js
+++ src/Constants/constant.js
@@ -1,5 +1,5 @@
 exports.apiURL = {
-  // baseURL: "https://api.travvolt.com",
+  
   // baseURL: "https://tourfinderbackend-production.up.railway.app",
    baseURL: "https://back.theskytrails.com",
   // baseURL: "http://localhost:8000",
