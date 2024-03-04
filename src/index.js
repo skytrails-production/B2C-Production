59aa6e6645edd61003c1807c7106ca2bd9b967d4
Qamar Ali <qamarali205@gmail.com>
2024-03-04 10:39:51 +0530

    updated clarity scripts

--- src/index.js
+++ src/index.js
@@ -15,6 +15,17 @@ root.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <HelmetProvider>
+      <Helmet>
+          <script type="text/javascript">
+            {`
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "layvxza6fm");
+            `}
+          </script>
+        </Helmet>
         <BrowserRouter>
           <App />
         </BrowserRouter>
