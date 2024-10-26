ce946b3d298a6ebdb6c7cd4737eced15f60b506c
Qamar Ali <qamarali205@gmail.com>
2024-10-26 16:32:42 +0530

    update code

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx}",],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Adjust if you're using TypeScript
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          900: '#1a202c', // Replace with your desired color code
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
