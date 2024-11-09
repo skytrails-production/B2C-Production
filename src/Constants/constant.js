1dc216ce3e1062d6c66ebb62cd4e552b3b51284d
Qamar Ali <qamarali205@gmail.com>
2024-11-09 17:47:04 +0530

    live

--- src/Constants/constant.js
+++ src/Constants/constant.js
@@ -2,8 +2,8 @@ exports.apiURL = {
   // baseURL: "https://api.travvolt.com",
   // baseURL: "https://tourfinderbackend-production.up.railway.app",
 
-  // baseURL: "https://back.theskytrails.com",
-   baseURL: "http://localhost:8000",
+  baseURL: "https://back.theskytrails.com",
+  //  baseURL: "http://localhost:8000",
 
   flightEmail: "shivam@theskytrails.com",
   phoneNo: "9999479696",
